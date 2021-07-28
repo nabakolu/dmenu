9d537739c5667e80e5a830f2966a4e4892de2daa
intenso arch <lnabakowski@mailo.com>
2021-07-28 19:34:56 +0200

    Changed to blobmoji

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Roboto Mono Nerd Font:size=10",
-	"Noto Color Emoji:size=10"
+	"Blobmoji:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
